0e60ef0270931e534246ea9a4b4c237cdd6aaf0f
Nobuhisa Koizumi <nobuhisa.oss@gmail.com>
2013-03-27 22:46:50 +0900

    サンプルプロジェクトを追加

---
+++ DocSample/Library1.fs
@@ -0,0 +1,10 @@
+﻿namespace DocSample
+
+module Sample =
+    /// あいさつ関数です。
+    let hello () = printfn "hello, world"
+
+    /// サンプルクラスです。
+    type Sample() =
+        /// サンプルメソッドです。
+        member this.Sample () = printfn "F#!F#!"
---
+++ DocSample/Script.fsx
@@ -0,0 +1,37 @@
+﻿(**
+THIS IS A SAMPLE
+================
+吾輩はサンプルである。名前はまだない。
+
+Sub Title
+---------
+- sakurai
+- suzuki
+- _tahara_
+- nakagawa
+
+`コミュニティ`
+
+- [F# User Group - Japan](https://github.com/Nobuhisa/FSUG_JP/wiki)
+
+*)
+
+(*** hide ***)
+// この部分を隠したい時
+#load "Library1.fs"
+open DocSample
+
+(**
+### Usage:
+この素晴らしいサンプルは以下のように使用します。
+*)
+Sample.hello ()
+
+let s = Sample.Sample()
+s.Sample()
+
+(**
+おわりに
+-------
+Good job, Thomas!
+*)
